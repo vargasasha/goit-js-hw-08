e659e6dd3573d6550b5b0f66e1e0ef3d954c2e32
Oleksandr Varga <vargasasha@gmail.com>
2023-04-20 16:09:40 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new player(iframe);
+
+iframePlayer.on(
+  'timeupdate',
+  throttle(event => {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(event.seconds)
+    );
+  }, 1000)
+);
+
+iframePlayer.setCurrentTime(
+  JSON.parse(localStorage.getItem('videoplayer-current-time')) ?? 0);
+
+// console.log(JSON.parse(localStorage.getItem('videoplayer-current-time')));
+
 
